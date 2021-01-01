f895490cb063227c4946a472a6e447c0a2a4a2d9
linh0303052 <ltt.lop9a1.lhlinh@gmail.com>
2021-01-01 22:32:09 +0700

    fix syntax error and

--- exercise/views.py
+++ exercise/views.py
@@ -37,9 +37,7 @@ def update_exercise(request):
                 new_record = JoinExercise(user = user, date = current_date)
                 new_record.save()
                 yester_of_current = current_date - timedelta(days=1)
-                if last_date.day == yester_of_current.day and
-                    last_date.month == yester_of_current.month and
-                    last_date.year == yester_of_current.year:
+                if (last_date.day == yester_of_current.day and last_date.month == yester_of_current.month and last_date.year == yester_of_current.year):
                     user.no_consecutive_day += 1
                 else:
                     user.no_consecutive_day = 1
